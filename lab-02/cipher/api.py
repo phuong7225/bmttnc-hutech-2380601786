2d5c573d3ee282e87aba9e3d34ba3c2267787dad
phuong <phuong7225@gmail.com>
2026-05-20 15:02:48 +0700

    [ADD] lab-02 railfence

--- lab-02/cipher/api.py
+++ lab-02/cipher/api.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 from caesar import CaesarCipher
 from vigenere import VigenereCipher
+from railfence import RailFenceCipher
 
 app = Flask(__name__)
 
 caesar_cipher = CaesarCipher()
 vigenere_cipher = VigenereCipher()
+railfence_cipher = RailFenceCipher()
 
 @app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
@@ -37,6 +39,22 @@ def vigenere_decrypt():
     plaint_text = data['plain_text']
     key = data['key']
     decrypted_text = vigenere_cipher.decrypt_text(plaint_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+@app.route("/api/railfence/encrypt", methods=["POST"])
+def railfence_encrypt():
+    data = request.json
+    plaint_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.encrypt_text(plaint_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def railfence_decrypt():
+    data = request.json
+    plaint_text = data['plain_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.decrypt_text(plaint_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
 #main funtion
